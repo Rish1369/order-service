562439871484697bf6be16e785175f2322e75937
Rishabh Rai <rishabhrai5649@gmail.com>
2026-04-13 22:52:26 +0530

    complete with the openfeign and interservice communication

--- src/main/java/com/rish/microservices/order_service/service/OrderService.java
+++ src/main/java/com/rish/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.rish.microservices.order_service.service;
 
+import com.rish.microservices.order_service.client.InventoryClient;
 import com.rish.microservices.order_service.dto.OrderRequest;
 import com.rish.microservices.order_service.model.Order;
 import com.rish.microservices.order_service.repository.OrderRepository;
@@ -11,16 +12,25 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-
+    private final InventoryClient inventoryClient;
     private final OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest){
-        //map order request to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        //save order to orderRepository
-        orderRepository.save(order);
+
+        var isProductInStock = inventoryClient.inInStock(orderRequest.skuCode() , orderRequest.quantity());
+
+        if(isProductInStock){
+            //map order request to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to orderRepository
+            orderRepository.save(order);
+        }
+        else{
+            throw new RuntimeException("Product is not in stock");
+        }
+
     }
 }
--- src/test/java/com/rish/microservices/order_service/OrderServiceApplicationTests.java
+++ src/test/java/com/rish/microservices/order_service/OrderServiceApplicationTests.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+
 class OrderServiceApplicationTests {
 
 	@Autowired
